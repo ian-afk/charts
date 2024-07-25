ff9cad4f149d257f9e2ae5b3b06d5927d00e2138
ian-afk <68414797+ian-afk@users.noreply.github.com>
2024-07-25 19:15:36 +0800

    add combinedChart

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import LineChartComp from "./components/LineChartComp";
 import BarChartComp from "./components/BarChartComp";
 import PieChartSample from "./components/PieChartSample";
+import CombinedChart from "./components/CombinedChart";
 function App() {
   return (
     <>
       <LineChartComp />
       <BarChartComp />
       <PieChartSample style={{ outline: "none" }} />
+      <CombinedChart />
     </>
   );
 }
